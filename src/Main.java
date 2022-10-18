9d991b008d0c8405e106520a25fe810b1a6f240e
User <333@fd.com>
2022-10-18 20:21:23 +0400

    Сделал всё, что смог(

--- src/Main.java
+++ src/Main.java
@@ -16,25 +16,25 @@ public class Main {
         System.out.println(fullName);
 
         System.out.println("Задача №5");//Четвертую задачу не неашел
-        int lastN = fullName.indexOf(" ");
-        int middleN = fullName.lastIndexOf(" ");
-        String lastName2 = fullName.substring(0, lastN);
-        String firstName2 = fullName.substring(lastN, middleN);
-        String middleName2 = fullName.substring(middleN, fullName.length());
+        String lastName2 = fullName.substring(0, fullName.indexOf(" "));
+        String firstName2 = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" "));
+        String middleName2 = fullName.substring(fullName.lastIndexOf(" "), fullName.length());
         System.out.println("Фамилия сотрудника - " + lastName2 + "\n" + "Имя сотрудника -" + firstName2 + "\n" + "Отчество сотрудника -" + middleName2);
         //какой способ лучше не пойму
         String[] word = fullName.split(" ");
         System.out.println("Фамилия сотрудника - " + word[0]);
-        System.out.println("Имя сотрудника - " + word[2]);
-        System.out.println("Отчество сотрудника - " + word[1]);
+        System.out.println("Имя сотрудника - " + word[1]);
+        System.out.println("Отчество сотрудника - " + word[2]);
 
         System.out.println("Задача №6");
-        String fullName1 = "иванов иван иванович";
-        int x = fullName1.indexOf(" " +1);
-        int y = fullName1.lastIndexOf(" " + 1);
+        String fullName1 = "толстой лев николаевич";
+        int x = fullName1.indexOf(" ") + 1; // создавать переменную х и у или сразу значение переменных записать в индекс массива с?
+        int y = fullName1.lastIndexOf(" ") + 1;
         char[] c = fullName1.toCharArray();
-//        fullName1 = Character.toUpperCase(0);???
-
+        c[0] = Character.toUpperCase(c[0]);
+        c[x] = Character.toUpperCase(c[x]);
+        c[y] = Character.toUpperCase(c[y]);
+        System.out.println(c);
 
 
 
